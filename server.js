64aa6414c26b5128146b0c743736250f6ae78cab
nickciesla <nicholasciesla@gmail.com>
2018-07-02 12:26:17 -0400

    add index.html

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('node')
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
