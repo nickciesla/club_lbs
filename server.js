9c2499183e4eba8ff4c8fe8f4fafc67233379ea9
nickciesla <nicholasciesla@gmail.com>
2018-07-02 13:40:56 -0400

    add ejs

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
+const mongo_url = 'mongodb://club_lbs_admin:club_lbs_admin_1@ds125381.mlab.com:25381/club_lbs'
 
-app.listen(3000, () => {
-  console.log('listening on 3000')
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect(mongo_url, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('club_lbs') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  db.collection('quotes').find().toArray((err, results) => {
+    if (err) return console.log(err)
+    console.log(results)
+    res.render('index.ejs', {quotes: result})
+  })
 })
 
 app.post('/quotes', (req, res) => {
-  console.log('quote')
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
